57db190c941cb96eca732cf2458836b59e1b9ada
Lucas Floriano da Silva <2022210220006@iesp.edu.br>
2022-09-07 16:49:20 -0300

    aula_python3.py

---
+++ aulas3/aula3.py
@@ -0,0 +1,25 @@
+# Questão 1
+mensagem = "eu amo programar em python!"
+
+print(mensagem)
+
+# Questão 2
+
+nome = input('informe seu nome por favor:')
+
+print(f"caro {nome.upper()} venho desejar a você um grandioso bom dia, e que seu dia seja abençoado.")
+
+#Questão 3
+
+numero1 = int(input('informe um número de 1 a 10:'))
+resultado_soma = (numero1 + 1357)
+resultado_multiplicacao = (resultado_soma * 8)
+resultado_divisao = (resultado_multiplicacao / 5)
+resultado_elevacao = (resultado_divisao ** 2)
+print('a soma deu', resultado_soma, ',a multiplicação deu,', resultado_multiplicacao, ',a divisão resultou em', resultado_divisao, 'e o resultado da elevação foi:', resultado_elevacao,)
+
+
+# Questão 4
+
+nome = input("digite seu nome por favor:")
+print(f"caro {nome.upper()} venho por meio deste lhe informar da minha festa de aniversário as 19h")
