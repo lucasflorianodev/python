1f637df1eb0a2d6221dcb2256cbbbf589b168b46
Lucas Floriano da Silva <112184116+lucasflorianodev@users.noreply.github.com>
2022-10-04 21:06:34 -0300

    quinto commit

---
+++ aulas9/aulas9.py
@@ -0,0 +1,24 @@
+# Questão 1 - Use um dicionário para armazenar informações sobre uma pessoa que você conheça. 
+#Armazene seu primeiro nome, o sobrenome, a idade e a cidade em que ela vive. 
+#Você deverá ter chaves como primeiro_nome, sobrenome, idade, e cidade. Por fim,
+#mostre cada informação armazenada em seu dicionário.
+
+Lucas = {"Primeiro nome": "Lucas", "Sobrenome": "Floriano", "idade": 18, "cidade": "João Pessoa-PB"}
+print(Lucas["Primeiro nome"])
+print(Lucas["Sobrenome"])
+print(Lucas["idade"])
+print(Lucas["cidade"])
+
+
+#Questão 2 - Use um dicionário para armazenar os números favoritos de algumas pessoas. Esoclha cinco colegas,
+#e pergunte quais seus número favoritos. 
+#Use seus nomes para serem as chaves de cada número favorito.
+#Ao final, exiba o nome de cada pessoas e seu número favorito.
+
+Número = {"lucas": 23, "ricardo": 54, "diego": 33, "rossana": 87, "bruna": 18}
+
+print(Número["lucas"])
+print(Número["ricardo"])
+print(Número["diego"])
+print(Número["rossana"])
+print(Número["bruna"])
